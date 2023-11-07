2ab763031dad1596d6853e63c3dfe6c0ce37212e
Andressa Viana Batista <andressavianab@gmail.com>
2023-11-07 19:34:55 -0300

    added hash

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 let user = require("../models/User");
 let moongose = require("mongoose");
+const bcrypt = require('bcrypt');
 
 const User = moongose.model("User", user);
 
 router.post("/user", async (req, res) => {
   const { name, email, password } = req.body;
+  const saltRounds = 10;
 
   if (!name || !email || !password) {
     res.status(400).json({ error: "Please fill in all fields!" });
@@ -17,14 +19,16 @@ router.post("/user", async (req, res) => {
     const user = await User.findOne({email: email});
 
     if(user == undefined) {
-      const newUser = new User({
-        name: name,
-        email: email,
-        password: password,
-      });
-      await newUser.save();
-      res.status(200).json({ email: email, message: "User created succesfully" });
-      return;
+      bcrypt.hash(password, saltRounds, async (err, hash) => {
+        const newUser = new User({
+          name: name,
+          email: email,
+          password: hash,
+        });
+        await newUser.save();
+        res.status(200).json({ email: email, message: "User created succesfully" });
+        return;
+    });
     } else {
       res.status(400).json({error: "E-mail alredy registered."})
       return;
