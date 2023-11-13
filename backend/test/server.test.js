615f26f2f2ad251db8b760cff1fcd4f59a94f592
Andressa Viana Batista <andressavianab@gmail.com>
2023-11-13 15:50:02 -0300

    Organizing the project structure, moving files to the backend folder

---
+++ backend/test/server.test.js
@@ -0,0 +1,15 @@
+let app = require("../src/app.js");
+let supertest = require("supertest");
+let request = supertest(app); //request object
+
+test("must run on port 3000", () => {
+  return request
+    .get("/")
+    .then((res) => {
+      let status = res.statusCode;
+      expect(status).toEqual(200);
+    })
+    .catch((err) => {
+      console.log(err)
+    });
+});
